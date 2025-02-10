03a79e0dd32131fe616cb7d8001decac56e1db62
tuantuan <tuantuan@wallace.com>
2025-02-11 00:55:32 +0800

    debug

--- langchain/0_tools.py
+++ langchain/0_tools.py
@@ -1,10 +1,20 @@
 import os
 import chromadb
 
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i+n]
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-persistent_dir = os.path.join(current_dir, "db", "chroma_db")
-client = chromadb.PersistentClient(path=persistent_dir)
 
-collection = client.get_collection("langchain")
-print(collection.peek(1).popitem())
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# persistent_dir = os.path.join(current_dir, "db", "chroma_db")
+# client = chromadb.PersistentClient(path=persistent_dir)
+
+# collection = client.get_collection("langchain")
+# print(collection.peek(1).popitem())
+
+if __name__ == "__main__":
+    lst = [1,2,3,4,5]
+    c = chunks(lst, 3)
+    print(list(c))
+    print([lst[i:i+2]] for i in range(0, len(lst), 2))
--- langchain/11_rag_basic.py
+++ langchain/11_rag_basic.py
@@ -1,13 +1,13 @@
 import os
 
 from dotenv import load_dotenv
-from langchain.text_splitter import TokenTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "books", "us_bill_of_rights.txt")
+file_path = os.path.join(current_dir, "books", "odyssey.txt")
 persistent_dir = os.path.join(current_dir, "db", "chroma_db")
 
 load_dotenv()
@@ -27,7 +27,7 @@ if not os.path.exists(persistent_dir):
 
     # 切分文件
     # 注意 chunk_size 和 chunk_overlap 两个参数
-    text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
 
     print("\n---- 文件Chunk信息----\n")
@@ -36,10 +36,16 @@ if not os.path.exists(persistent_dir):
     # 创建embedding
     embeddings = OpenAIEmbeddings(model=embedding_model)
 
+    # 对docs进行分片，防止单次数量过大导致调用大模型接口失败
+    batch_size = 4
+    docs_list = list(docs[i:batch_size] for i in range(0, len(docs), batch_size))
+    print(f"批量docsList的数量： {len(docs_list)}")
+
     # 创建向量数据库
-    db = Chroma.from_documents(
-        docs, embeddings, persist_directory=persistent_dir
-    )
+    vectorstore = Chroma(collection_name="langchain", embedding_function=embedding_model, persist_directory=persistent_dir)
+    for i in range(0, len(docs_list)):
+        print(f"====> size of doc: {len(docs_list[i])}")
+        vectorstore.add_documents(docs_list[i])
     print("\n 创建向量数据库完成")
 
 else:
